1650d0926788397e5c88bb3471e71aa400afd911
Suhail aslam <suhailaslam81@gmail.com>
2025-11-06 10:10:46 +0530

    Time: 2 ms (47.06%) | Memory: 55.7 MB (52.66%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    const array =[]
+    for(let i=0; i<s.length; i++){
+        array[indices[i]]=s[i]
+        
+    } return array.join("")
+};
