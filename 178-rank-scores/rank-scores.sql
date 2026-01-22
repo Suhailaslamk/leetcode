75854e326319737cd5de69d7ca1f909db5d8cb34
Suhail aslam <suhailaslam81@gmail.com>
2026-01-22 09:38:14 +0530

    Time: 341 ms (88.59%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,6 +1,6 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 SELECT
     score,
-    DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+    DENSE_RANK() OVER (ORDER BY score DESC) AS rank
 FROM Scores
 ORDER BY score DESC;
