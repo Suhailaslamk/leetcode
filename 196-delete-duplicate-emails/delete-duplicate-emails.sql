2f8e0621c8e9814a899237f64907a23f901b2aed
Suhail aslam <suhailaslam81@gmail.com>
2026-01-24 14:52:37 +0530

    Time: 220 ms (94.66%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,6 +1,9 @@
-/* Write your T-SQL query statement below */
-DELETE p1
-FROM Person p1
-JOIN Person p2
-  ON p1.email = p2.email
- AND p1.id > p2.id;
+-- Write your PostgreSQL query statement below
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT min_id FROM (
+        SELECT MIN(id) AS min_id
+        FROM Person
+        GROUP BY email
+    ) t
+);
