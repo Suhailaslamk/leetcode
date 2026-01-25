4e32d832c159b6067768fc8996ea3105d6b1578b
Suhail aslam <suhailaslam81@gmail.com>
2026-01-25 12:09:09 +0530

    Time: 591 ms (18.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,14 @@
+/* Write your T-SQL query statement below */
+SELECT 
+ Department ,
+ Employee ,
+ Salary 
+FROM (
+    SELECT d.name AS Department,e.name AS Employee ,e.salary AS Salary ,DENSE_RANK() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC)  AS salary_Rank
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
+) ranked
+
+
+ WHERE salary_Rank <= 3;
+ 
