fd6e5b86299d8fb6bb07a2888d11a98e6b405ddf
Suhail aslam <suhailaslam81@gmail.com>
2026-02-11 09:42:36 +0530

    Time: 486 ms (61.99%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,3 +1,6 @@
--- Write your PostgreSQL query statement below
-SELECT player_id , MIN(event_date)  AS first_login FROM Activity
-GROUP BY player_id
+/* Write your PL/SQL query statement below */
+SELECT 
+    player_id,
+    TO_CHAR(MIN(event_date), 'YYYY-MM-DD') AS first_login
+FROM Activity
+GROUP BY player_id;
