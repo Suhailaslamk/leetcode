e8029e43f08e3ce937b3f881cc5acc3e134f1075
Suhail aslam <suhailaslam81@gmail.com>
2026-02-13 09:50:52 +0530

    Time: 397 ms (22.17%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 SELECT MAX(num)  AS num FROM MyNumbers 
 WHERE num IN (SELECT  num FROM MyNumbers 
 GROUP BY num 
