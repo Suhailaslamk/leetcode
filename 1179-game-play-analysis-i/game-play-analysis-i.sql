34bca21d76656d80d45968d17483f70c3daa2aa8
Suhail aslam <suhailaslam81@gmail.com>
2026-02-19 09:26:07 +0530

    Time: 820 ms (5.72%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,6 +1,3 @@
-/* Write your PL/SQL query statement below */
-SELECT 
-    player_id,
-    TO_CHAR(MIN(event_date), 'YYYY-MM-DD') AS first_login
-FROM Activity
-GROUP BY player_id;
+# Write your MySQL query statement below
+SELECT player_id , MIN(event_date)  AS first_login FROM Activity
+GROUP BY player_id
