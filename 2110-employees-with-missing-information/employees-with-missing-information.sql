b7cc7018b38d1ae894bf499098f450525eb62c37
Suhail aslam <suhailaslam81@gmail.com>
2026-03-19 15:57:39 +0530

    Time: 199 ms (73.97%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,4 +1,4 @@
-/* Write your T-SQL query statement below */
+-- Write your PostgreSQL query statement below
 SELECT COALESCE(e.employee_id,s.employee_id) AS employee_id FROM  Employees e
 FULL OUTER JOIN Salaries s ON e.employee_id = s.employee_id
 WHERE s.salary IS NULL OR e.name IS NULL
