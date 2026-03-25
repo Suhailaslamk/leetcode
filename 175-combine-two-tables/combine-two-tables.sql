f115ca8cf4bff4493e4022e7257d0cd3098407cc
Suhail aslam <suhailaslam81@gmail.com>
2026-03-25 10:18:48 +0530

    Time: 444 ms (78.04%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,3 +1,3 @@
-/* Write your T-SQL query statement below */
-SELECT p.firstName ,p.lastName ,a.city ,a.state  FROM Person p 
-LEFT JOIN Address a ON p.personId = a.personId 
+/* Write your PL/SQL query statement below */
+SELECT p.firstname, p.lastname,a.city,a.state FROM Person p 
+LEFT JOIN Address a ON p.personId = a.personId
