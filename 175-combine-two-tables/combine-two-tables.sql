570a2fee772d5ff397b9790bb89ccdc90ecd9be5
Suhail aslam <suhailaslam81@gmail.com>
2026-05-09 10:32:08 +0530

    Time: 447 ms (21.68%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,3 +1,4 @@
-/* Write your PL/SQL query statement below */
-SELECT p.firstname, p.lastname,a.city,a.state FROM Person p 
-LEFT JOIN Address a ON p.personId = a.personId
+# Write your MySQL query statement below
+SELECT p.firstName , p.lastName , a.city , a.state  FROM 
+Person p 
+LEFT JOIN  Address a ON p.personId  = a.personId 
