89558ecfe8e5211d74c5ebc81e4e2733cc838617
Suhail aslam <suhailaslam81@gmail.com>
2026-05-27 15:02:24 +0530

    Time: 590 ms (45.99%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,15 +1,5 @@
 /* Write your T-SQL query statement below */
--- SELECT ROUND(SUM(tiv_2016 ), 2) 
---  FROM Insurance 
--- WHERE 
--- tiv_2015 IN (SELECT tiv_2015 FROM Insurance 
--- GROUP BY tiv_2015
--- HAVING COUNT(*) > 1 )
--- AND NOT EXISTS (
---         SELECT 1 
---         FROM Insurance i
---         WHERE i.lan <> 
--- )
+
 
 
 
