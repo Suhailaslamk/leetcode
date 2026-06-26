81dd166017b6338588f01acc02c62ce3b8b63cc4
Suhail aslam <suhailaslam81@gmail.com>
2026-06-26 22:56:24 +0530

    Time: 262 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+WITH AllFriends AS (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+)
+SELECT TOP 1
+    id, 
+    COUNT(*) AS num
+FROM AllFriends
+GROUP BY id
+ORDER BY num DESC;
